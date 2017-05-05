a44d738e0d92f2b36752b4eb83a3babd1c59e162
Tim Humphries <tim.humphries@ambiata.com>
2017-05-05 11:18:14 +1000

    Generate Purescript types

--- machinator-purescript/src/Machinator/Purescript/Scheme/Types.hs
+++ machinator-purescript/src/Machinator/Purescript/Scheme/Types.hs
@@ -1,3 +1,84 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Machinator.Purescript.Scheme.Types where
+
+
+import qualified Data.Char as Char
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import           Data.Set (Set)
+import qualified Data.Set as S
+import qualified Data.Text as T
+
+import           Machinator.Core
+import qualified Machinator.Core.Graph as MG
+import           Machinator.Purescript.Data.Types
+import qualified Machinator.Purescript.Scheme.Types.Codegen as Codegen
+
+import           P
+
+import qualified System.FilePath.Posix as FilePath
+import           System.IO (FilePath)
+
+
+types :: PurescriptTypesVersion -> [DefinitionFile] -> Either PurescriptTypesError [(FilePath, Text)]
+types v ds =
+  case v of
+    PurescriptTypesV1 ->
+      typesV1 ds
+
+typesV1 :: [DefinitionFile] -> Either PurescriptTypesError [(FilePath, Text)]
+typesV1 dfs =
+  let DefinitionFileGraph fg = MG.buildFileGraph dfs
+      mg = M.mapKeys filePathToModuleName (fmap (S.map filePathToModuleName) fg)
+  in for dfs $ \(DefinitionFile fp defs) ->
+       let mn = filePathToModuleName fp in
+       pure (genFileName mn, renderModule mn mg defs)
+
+-- -----------------------------------------------------------------------------
+
+renderModule :: ModuleName -> Map ModuleName (Set ModuleName) -> [Definition] -> Text
+renderModule mn@(ModuleName n) imports defs =
+  T.unlines [
+      T.unwords ["module", n, "where"]
+    , maybe mempty (T.unlines . fmap renderImport . toList) (M.lookup mn imports)
+    , T.unlines . flip foldMap defs $ \def ->
+        Codegen.genTypesV1 def : maybeToList (Codegen.genUnwrapV1 def)
+    ]
+
+renderImport :: ModuleName -> Text
+renderImport (ModuleName n) =
+  "import " <> n
+
+-- -----------------------------------------------------------------------------
+
+newtype ModuleName = ModuleName {
+    _unModuleName :: Text
+  } deriving (Eq, Ord, Show)
+
+
+-- | Derive a module name from the relative 'FilePath'.
+--
+-- @
+-- λ> filePathToModuleName "./path_to/my/favourite_Template_place.hs"
+-- ModuleName {unModuleName = "PathTo.My.FavouriteTemplatePlace"}
+-- @
+--
+-- TODO V2 Accept an alternative function as a parameter.
+filePathToModuleName :: FilePath -> ModuleName
+filePathToModuleName =
+  ModuleName . T.pack . goUpper . FilePath.dropExtension
+  where
+    goUpper [] = []
+    goUpper (x:xs)
+      | Char.isAlphaNum x = Char.toUpper x : go xs
+      | otherwise = goUpper xs
+    go [] = []
+    go (x:xs)
+      | x == '/' = '.' : goUpper xs
+      | Char.isAlphaNum x = x : go xs
+      | otherwise = goUpper xs
+
+genFileName :: ModuleName -> FilePath
+genFileName (ModuleName n) =
+  T.unpack (T.replace "." "/" n) <> ".purs"
---
+++ machinator-purescript/src/Machinator/Purescript/Scheme/Types/Codegen.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Machinator.Purescript.Scheme.Types.Codegen (
+    genTypesV1
+  , genUnwrapV1
+  ) where
+
+
+import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+
+import           Machinator.Core
+import           Machinator.Core.Data.Definition
+
+import           P
+
+import           Text.PrettyPrint.Annotated.WL (Doc, (<+>), (<##>))
+import qualified Text.PrettyPrint.Annotated.WL as WL
+
+
+-- | Generates a type declaration for the given definition.
+genTypesV1 :: Definition -> Text
+genTypesV1 (Definition (Name n) dec) =
+  renderText $ case dec of
+    Variant (c1 :| cts) ->
+      WL.hang 2 $
+        string "data" <+> text n <##> string "="
+          <+> fold (WL.punctuate (WL.hardline <> string "| ") (fmap (uncurry genConstructorV1) (c1:cts)))
+    Record fts ->
+      WL.hang 2 $
+        string "newtype" <+> text n <+> string "=" <+> text n <+> genRecordV1 fts
+
+genTypeV1 :: Type -> Doc a
+genTypeV1 ty =
+  case ty of
+    Variable (Name n) ->
+      text n
+    GroundT g ->
+      text (unName (groundToName g))
+    ListT t2 ->
+      WL.parens (string "Array" <+> genTypeV1 t2)
+
+genConstructorV1 :: Name -> [Type] -> Doc a
+genConstructorV1 (Name n) tys =
+  text n <+> WL.hsep (fmap genTypeV1 tys)
+
+-- | Generates a naked record for the given definition.
+--
+-- @
+-- [(Name "foo", GroundT StringT), (Name "bar", GroundT StringT)]
+-- { foo :: String, bar :: String }
+-- @
+genRecordV1 :: [(Name, Type)] -> Doc a
+genRecordV1 fts =
+  WL.hang 2 $
+    WL.encloseSep (string "{") (string "}") (string ",") $
+      with fts $ \(Name n, ty) ->
+        text n <+> string "::" <+> genTypeV1 ty
+
+-- | Creates an unwrap function for records. This is a little stubborn as we target 0.9.3.
+--
+-- @
+-- newtype Hello = Hello { foo :: Boolean }
+-- unHello :: Hello -> { foo :: Boolean }
+-- unHello (Hello h) = h
+-- @
+genUnwrapV1 :: Definition -> Maybe Text
+genUnwrapV1 (Definition (Name n) dec) =
+  renderText <$> case dec of
+    Variant _ ->
+      empty
+    Record fs ->
+      Just $
+             text ("un" <> n) <+> string "::" <+> text n <+> "->" <+> genRecordV1 fs
+        <##> text ("un" <> n) <+> WL.parens (text n <+> string "x") <+> string "=" <+> string "x"
+
+-- -----------------------------------------------------------------------------
+
+string :: [Char] -> Doc a
+string =
+  WL.text
+
+text :: Text -> Doc a
+text =
+  WL.text . T.unpack
+
+renderText :: Doc a -> Text
+renderText =
+  TL.toStrict . WL.displayT . WL.renderPrettyDefault
