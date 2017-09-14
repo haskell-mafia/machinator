88b9f2c476c0c8f582c45501a8ed2cce94a84339
Tim Humphries <tim.humphries@ambiata.com>
2017-09-14 14:50:54 +1000

    Export things from machinator-purescript

--- machinator-purescript/src/Machinator/Purescript.hs
+++ machinator-purescript/src/Machinator/Purescript.hs
@@ -1,3 +1,11 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Machinator.Purescript where
+module Machinator.Purescript (
+    PurescriptTypesVersion (..)
+  , PurescriptTypesError (..)
+  , types
+  , typesV1
+  ) where
+
+import           Machinator.Purescript.Data.Types
+import           Machinator.Purescript.Scheme.Types
